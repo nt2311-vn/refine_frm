c6c9ede5f26031bfbf5e6943065c419de5164097
nt2311-vn <nguyentoan231196@gmail.com>
2024-07-14 10:33:49 +0700

    export all functions in components

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+import UpcomingEvents from "./home/upcoming-events";
+import DealsChart from "./home/deals-chart";
+import UpcomingEventsSkeleton from "./skeleton/upcoming-events";
+
+export { UpcomingEvents, DealsChart, UpcomingEventsSkeleton };
