80177fdb5dfe9ad6ef84f1a0e9f7064cdf050a4c
nt2311-vn <nguyentoan231196@gmail.com>
2024-07-14 23:24:27 +0700

    export function totalCountCard

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,11 @@
 import UpcomingEvents from "./home/upcoming-events";
 import DealsChart from "./home/deals-chart";
 import UpcomingEventsSkeleton from "./skeleton/upcoming-events";
+import DashboardTotalCountCard from "./home/total-count-card";
 
-export { UpcomingEvents, DealsChart, UpcomingEventsSkeleton };
+export {
+  UpcomingEvents,
+  DealsChart,
+  UpcomingEventsSkeleton,
+  DashboardTotalCountCard,
+};
