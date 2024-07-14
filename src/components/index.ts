8ca073cef5c96cf755ab5af18ae257450117f4fa
nt2311-vn <nguyentoan231196@gmail.com>
2024-07-15 02:31:59 +0700

    export LatestActivities for using

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,10 +2,12 @@ import UpcomingEvents from "./home/upcoming-events";
 import DealsChart from "./home/deals-chart";
 import UpcomingEventsSkeleton from "./skeleton/upcoming-events";
 import DashboardTotalCountCard from "./home/total-count-card";
+import LatestActivities from "./home/latest-activities";
 
 export {
   UpcomingEvents,
   DealsChart,
   UpcomingEventsSkeleton,
   DashboardTotalCountCard,
+  LatestActivities,
 };
