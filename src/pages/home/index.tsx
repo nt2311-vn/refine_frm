3da51a86f84c5cd77d3fc9604690aac0a934bcb3
nt2311-vn <nguyentoan231196@gmail.com>
2024-07-14 10:33:18 +0700

    home page components

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,3 +1,4 @@
+import { DealsChart, UpcomingEvents } from "@/components";
 import { Col, Row } from "antd";
 
 export const Home = () => {
@@ -5,10 +6,10 @@ export const Home = () => {
     <div>
       <Row gutter={[32, 32]} style={{ marginTop: "32px" }}>
         <Col xs={24} sm={24} xl={8} style={{ height: "460px" }}>
-          CalendarUpcomingEvents
+          <UpcomingEvents />
         </Col>
         <Col xs={24} sm={24} xl={8} style={{ height: "460px" }}>
-          DashboardDealCharts
+          <DealsChart />
         </Col>
       </Row>
     </div>
