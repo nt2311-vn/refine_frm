3eabb316ce545f7623f5ab9b09076fb01ac9e6ca
nt2311-vn <nguyentoan231196@gmail.com>
2024-07-14 10:02:45 +0700

    update all resources available in vertical navbar

--- src/config/resources.tsx
+++ src/config/resources.tsx
@@ -1,4 +1,8 @@
-import { DashboardOutlined, ShopOutlined } from "@ant-design/icons";
+import {
+  DashboardOutlined,
+  ProjectOutlined,
+  ShopOutlined,
+} from "@ant-design/icons";
 import { IResourceItem } from "@refinedev/core";
 
 export const resources: IResourceItem[] = [
@@ -17,5 +21,18 @@ export const resources: IResourceItem[] = [
       label: "Companies",
       icon: <ShopOutlined />,
     },
+    show: "/companies/:id",
+    create: "/companies/new",
+    edit: "/companies/edit/:id",
+  },
+  {
+    name: "tasks",
+    list: "/tasks",
+    meta: {
+      label: "Tasks",
+      icon: <ProjectOutlined />,
+    },
+    create: "/tasks/new",
+    edit: "/tasks/edit/:id",
   },
 ];
